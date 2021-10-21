90e22d5fd46aaabc0ae5bcb43ed22c9be4b9aed3
Leonardo1952 <lo88980@gmail.com>
2021-10-21 15:01:48 -0300

    country card

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'clock.dart';
+import 'country_card.dart';
 import 'time_in_hour_and_minute.dart';
 
 class Body extends StatelessWidget {
@@ -17,7 +17,30 @@ class Body extends StatelessWidget {
             style: Theme.of(context).textTheme.bodyText1,
           ),
           const TimeInHourAndMinute(),
+          const Spacer(),
           Clock(),
+          const Spacer(),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: const [
+                CountryCard(
+                  country: "New York, USA",
+                  timeZone: "+3 HRS | EST",
+                  iconSrc: "assets/icons/Liberty.svg",
+                  time: "9:20",
+                  period: "PM",
+                ),
+                CountryCard(
+                  country: "Sydney, AU",
+                  timeZone: "+19 HRS | AEST",
+                  iconSrc: "assets/icons/Sydney.svg",
+                  time: "1:20",
+                  period: "AM",
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
